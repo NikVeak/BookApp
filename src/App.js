49b67e306ed21e57341e3d835bdc11acc15039fc
GG_WP_EASY <82731664+NikVeak@users.noreply.github.com>
2023-07-02 13:24:46 +0300

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,18 +1,40 @@
-//import logo from './logo.svg';
 import './App.css';
 import Header from './Header.js';
-import React from 'react';
-import List from "./List.js";
+import React, {useEffect, useState} from 'react';
 import Footer from './Footer.js';
+import Pagination from "./Pagination.js";
+import axios from "axios";
+
+const fetchData = async () =>
+{
+    try
+    {
+        const apiKey = "AIzaSyCqi37mzRrzkBrDZDb0BX9_IarX5iMOT88";
+        const response = await
+            axios.get("https://www.googleapis.com/books/v1/volumes?q=search+terms"+ "&key=" + apiKey + "&maxResults=40 ");
+        console.log(response.data.items);
+        return response.data.items;
+    }catch (error)
+    {
+        console.error(error);
+    }
+};
 
 function App()
 {
-  return (
+    const [books, setBooks] = useState([]);
+    useEffect(()=>
+    {
+       const dataApi = async () => {
+           const res = await fetchData();
+           setBooks(res);
+       }
+       dataApi();
+    });
+    return (
     <div className="App">
             <Header/>
-        <div className="Basis">
-            <List/>
-        </div>
+        <Pagination pageLimit={10} books={7}/>
 
             <Footer/>
     </div>
---
+++ src/Pagination.js
@@ -0,0 +1,94 @@
+import React,{useState, useEffect} from "react";
+
+const Pagination = ({pageLimit, books}) =>
+{
+    const [currentPageNow, setCurrentPageNow] = useState(1);
+    const [currentPageBooks, setCurrentPageBooks] = useState([]);
+    const [pageNumberGroup, setPageNumberGroup] = useState([]);
+
+    useEffect(()=>
+    {
+       setCurrentPageBooks(getPageData());
+       setPageNumberGroup(getPageNumberGroup());
+        console.log("walk");
+    }, [books, currentPageNow]);
+
+    const nextPage = () => setCurrentPageNow(
+        (prev)=>prev+1
+    );
+
+    const previousPage = () => setCurrentPageNow(
+        (prev)=>prev-1
+    );
+
+    const changePageTo = (pageNumber) =>
+        setCurrentPageNow(pageNumber);
+
+    const getPageData = () =>
+    {
+        const startIndex = currentPageNow * pageLimit - pageLimit;
+        const endIndex = startIndex + pageLimit;
+        return books.slice(startIndex, endIndex);
+    }
+
+    const getPageNumberGroup = () =>
+    {
+        let start = Math.floor((currentPageNow - 1) / 3) * 3;
+        console.log(new Array(3).fill(" ").map((_, index) => start + index + 1));
+        return new Array(3).fill(" ").map((_, index) => start + index + 1);
+    }
+    return(
+        <div>
+            <h1 className="heading">Books in pagination</h1>
+            <ul className="books-container">
+                {
+                    currentPageBooks.map(({id})=>
+                    {
+                        return(
+                          <li key={id} className="book">
+                              {id}
+                          </li>
+                        );
+                    })
+                }
+            </ul>
+            <div className="page-num-container">
+                <button
+                    className={`page-change-btn ${currentPageNow === 1 ? "disabled" : ""}  `}
+                    disabled={currentPageNow === 1}
+                    onClick={previousPage}
+                >
+                    Previous
+                </button>
+                <ul className="page-num-container list-style-none">
+                    {pageNumberGroup.map((value, index) => {
+                        return (
+                            <li
+                                className={`page-number ${
+                                    currentPageNow === value ? "active" : ""
+                                } `}
+                                key={index}
+                                onClick={() => changePageTo(value)}
+                            >
+                                {value}
+                            </li>
+                        );
+                    })}
+                </ul>
+                <button
+                    disabled={currentPageNow === Math.floor(books.length / pageLimit)}
+                    className={`page-change-btn ${
+                        currentPageNow === Math.floor(books.length / pageLimit)
+                            ? "disabled"
+                            : ""
+                    }  `}
+                    onClick={nextPage}
+                >
+                    Next
+                </button>
+            </div>
+        </div>
+    );
+}
+
+export default Pagination;

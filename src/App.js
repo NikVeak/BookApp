5963c3d25985ba0f7d7b6e0088f878cee624d829
GG_WP_EASY <82731664+NikVeak@users.noreply.github.com>
2023-07-02 10:52:04 +0300

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,14 +1,22 @@
 //import logo from './logo.svg';
 import './App.css';
-import Header from './header.js';
+import Header from './Header.js';
+import React, { Component }  from 'react';
+import List from "./List.js";
 
 function App()
 {
   return (
     <div className="App">
-      <Header></Header>
+        <div>
+            <Header/>
+        </div>
+        <div className="Basis">
+            <List/>
+        </div>
     </div>
   );
 }
 
 export default App;
+
---
+++ src/List.js
@@ -0,0 +1,22 @@
+import React from "react"
+
+class ulComponent
+{
+    Name = "ulBooks";
+
+}
+class List extends React.Component
+{
+
+    Name = "ListName";
+    render()
+    {
+        return
+        (
+            <ul>
+
+            </ul>
+        );
+    }
+}
+export default List;
